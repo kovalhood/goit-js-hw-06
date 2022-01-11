9f05ab28df63e2f7b02cd586852069600f8ac8fa
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-01-11 18:37:38 +0200

    Task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients')
+
+const ingredientsArray = ingredients.map(element => {
+  const ingredientItem = document.createElement('li');
+  ingredientItem.classList.add('item');
+
+  const ingredientName = document.createElement('p');
+  ingredientName.textContent = element;
+  ingredientItem.appendChild(ingredientName);
+
+  return ingredientItem;
+});
+
+ingredientsList.append(...ingredientsArray);
