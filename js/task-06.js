c9371ca03b22bca9dda52b57bdcab2b955e18103
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-01-12 21:32:56 +0200

    Task-6

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const inputEl = document.querySelector("input");
+console.log(inputEl);
+
+inputEl.addEventListener("blur", symbolsCheck);
+
+function symbolsCheck() {
+  const correctLength = this.dataset.length;
+  const inputDataLength = this.value.length;
+
+  if (correctLength == inputDataLength) {
+    this.style.borderColor = "#4caf50";
+  } else {
+    this.style.borderColor = "#f44336";
+  }
+}
